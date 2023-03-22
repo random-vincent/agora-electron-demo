7f7e3c9b73532900caf9072e8e7a91580bce8f4a
Vincent.An <anzhaowei@agora.io>
2023-03-22 14:30:36 +0800

    update config

--- renderer.js
+++ renderer.js
@@ -1,4 +1,10 @@
+const os = require('os');
+const path = require('path');
+
 const AgoraRtcEngine = require('agora-electron-sdk').default
+
+const sdkLogPath = path.resolve(os.homedir(),"./test.log") 
+const addOnPath = path.resolve(os.homedir(),"./addon.log") 
 
 // 填入你的 App ID
 const APPID = "";
@@ -6,8 +12,12 @@ const APPID = "";
 const token = null;
 
 let rtcEngine = new AgoraRtcEngine()
+
 // 初始化 AgoraRtcEngine 实例
 rtcEngine.initialize(APPID)
+
+rtcEngine.setLogFile(sdkLogPath)
+rtcEngine.setAddonLogFile(addOnPath)
 
 rtcEngine.on('error', (err, msg) => {
   console.log("Error!")
